99b06db6f6a1c3e26b9b0e8cfc6186a4da5fa647
Waldemar Porscha <wp@porscha.eu>
2023-04-07 23:14:32 +0200

    Update install scripts for Mac OS and Linux

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -11,16 +11,22 @@ New-Item -Path release/tools -ItemType Directory | Out-Null
 
 # build publish version of OpenKNXproducer
 dotnet.exe build OpenKNXproducer.csproj
-dotnet.exe publish OpenKNXproducer.csproj -c Debug -r win-x64 --self-contained true /p:PublishSingleFile=true
-dotnet.exe publish OpenKNXproducer.csproj -c Debug -r win-x86 --self-contained true /p:PublishSingleFile=true
+dotnet.exe publish OpenKNXproducer.csproj -c Debug -r win-x64   --self-contained true /p:PublishSingleFile=true
+dotnet.exe publish OpenKNXproducer.csproj -c Debug -r win-x86   --self-contained true /p:PublishSingleFile=true
+dotnet.exe publish OpenKNXproducer.csproj -c Debug -r osx-x64   --self-contained true /p:PublishSingleFile=true
+dotnet.exe publish OpenKNXproducer.csproj -c Debug -r linux-x64 --self-contained true /p:PublishSingleFile=true
 
 # we copy publish version also to our bin to ensure same OpenKNXproducer for our delivered products
-Copy-Item bin/Debug/net6.0/win-x64/publish/OpenKNXproducer.exe ~/bin/OpenKNXproducer-x64.exe
-Copy-Item bin/Debug/net6.0/win-x86/publish/OpenKNXproducer.exe ~/bin/OpenKNXproducer-x86.exe
+Copy-Item bin/Debug/net6.0/win-x64/publish/OpenKNXproducer.exe   ~/bin/OpenKNXproducer-x64.exe
+Copy-Item bin/Debug/net6.0/win-x86/publish/OpenKNXproducer.exe   ~/bin/OpenKNXproducer-x86.exe
+Copy-Item bin/Debug/net6.0/osx-x64/publish/OpenKNXproducer   ~/bin/OpenKNXproducer-osx64.exe
+Copy-Item bin/Debug/net6.0/linux-x64/publish/OpenKNXproducer ~/bin/OpenKNXproducer-linux64.exe
 
 # copy package content 
-Copy-Item ~/bin/OpenKNXproducer-x64.exe release/tools
-Copy-Item ~/bin/OpenKNXproducer-x86.exe release/tools
+Copy-Item ~/bin/OpenKNXproducer-x64.exe     release/tools
+Copy-Item ~/bin/OpenKNXproducer-x86.exe     release/tools
+Copy-Item ~/bin/OpenKNXproducer-osx64.exe   release/tools
+Copy-Item ~/bin/OpenKNXproducer-linux64.exe release/tools
 Copy-Item ~/bin/bossac.exe release/tools
 Copy-Item scripts/bossac-LICENSE.txt release/tools
 
