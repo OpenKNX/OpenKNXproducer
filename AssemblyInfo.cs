56e7d2f0397f3e6a3732c34c6e530b37002617f7
Waldemar Porscha <wp@porscha.eu>
2023-04-14 18:29:55 +0200

    Increase version

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenKNX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.13.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.13")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.14.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.14")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenKNXproducer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenKNXproducer")]
-[assembly: System.Reflection.AssemblyVersionAttribute("2.0.13.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("2.0.14.0")]
