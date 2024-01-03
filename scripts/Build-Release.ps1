8baaf46c269f9e80d63683c856e2adc6a946a7c1
Erkan Çolak <erkanc@gmx.de>
2024-01-03 09:54:38 +0100

    Update: Work in Progress
    Add platform-dependent function to retrieve the dotnet binary
    Replace OpenKNX Logo with updated version
    Implement function to fetch OpenKNXproducer version from the executable

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -1,24 +1,30 @@
-function OpenKNX_ShowLogo($AddCustomText= $null) {
+
+# This script builds the OpenKNXproducer release package for Windows, MacOS and Linux
+
+# To Show OpenKNX Logo in the console output
+function OpenKNX_ShowLogo($AddCustomText = $null) {
   Write-Host ""
   Write-Host "Open " -NoNewline
-  #Write-Host "▀" -ForegroundColor Green
-  Write-Host "$( [char]::ConvertFromUtf32(0x2580) )" -ForegroundColor Green
-  $unicodeString = "$( [char]::ConvertFromUtf32(0x2533) )$( [char]::ConvertFromUtf32(0x2501) )$( [char]::ConvertFromUtf32(0x2501) )$( [char]::ConvertFromUtf32(0x2501) )$( [char]::ConvertFromUtf32(0x2501) )$( [char]::ConvertFromUtf32(0x253B) ) "
-  
+  #Write-Host "■" -ForegroundColor Green
+  Write-Host "$( [char]::ConvertFromUtf32(0x25A0) )" -ForegroundColor Green
+  $unicodeString = "$( [char]::ConvertFromUtf32(0x252C) )$( [char]::ConvertFromUtf32(0x2500) )$( [char]::ConvertFromUtf32(0x2500) )$( [char]::ConvertFromUtf32(0x2500) )$( [char]::ConvertFromUtf32(0x2500) )$( [char]::ConvertFromUtf32(0x2534) ) "
+
   if ($AddCustomText) { 
-    #Write-Host "┳━━━━┻  $AddCustomText" -ForegroundColor Green
-    Write-Host "$($unicodeString) $($AddCustomText)"  -ForegroundColor Green
-  } else {
-    #Write-Host "┳━━━━┻" -ForegroundColor Green
-    Write-Host "$($unicodeString)"  -ForegroundColor Green
+      #Write-Host "┬────┴  $AddCustomText" -ForegroundColor Green
+      Write-Host "$($unicodeString) $($AddCustomText)"  -ForegroundColor Green
+  }
+  else {
+      #Write-Host "┬────┴" -ForegroundColor Green
+      Write-Host "$($unicodeString)"  -ForegroundColor Green
   }
 
-  #Write-Host "▄" -NoNewline -ForegroundColor Green
-  Write-Host "$( [char]::ConvertFromUtf32(0x2584) )" -NoNewline -ForegroundColor Green
+  #Write-Host "■" -NoNewline -ForegroundColor Green
+  Write-Host "$( [char]::ConvertFromUtf32(0x25A0) )" -NoNewline -ForegroundColor Green
   Write-Host " KNX"
   Write-Host ""
 }
 
+# To check on which OS we are running
 function CheckOS {
   # check on which os we are running
   # After check, the Os-Informations are availibe in the PS-Env.
@@ -49,59 +55,95 @@ function CheckOS {
   if($true) { Write-Host -ForegroundColor Green "- We are on '$CurrentOS' Build Environment with PowerShell $PSVersion"  ([Char]0x221A) }
   return $CurrentOS
 }
+# To check and install (Optional and for testing) dotnet if not exists
+function Test-AndInstallDotnet {
+  param (
+      [bool]$Install = $false
+  )
+  $dotnetExecutable = "dotnet.exe"
+  if ($IsMacOSEnv -or $IsLinuxEnv) {
+    $dotnetExecutable = "dotnet"
+    if (!(Test-Path -Path /usr/local/share/dotnet/dotnet)) {
+      # Ask user if we should install dotnet
+      if($Install) {
+        $installDotnet = Read-Host -Prompt "donet installation not found. Do you want to install dotnet? (y/n)"
+        if($installDotnet -eq "y") {
+          Write-Host "Installing dotnet..."
+          $downloadUrl = ""
+          $tarCommand = ""
+          if ($IsMacOSEnv) {
+            $downloadUrl = "https://download.visualstudio.microsoft.com/download/pr/2b5a6c4c-5f2b-4f2b-8f1a-2c9f8f4c9f2b/2e4c6d0b0b0d0f2d4f2b0f2d4c0f4b0f/dotnet-sdk-6.0.100-rc.2.21505.57-osx-x64.tar.gz"
+            $tarCommand = "tar zxf dotnet-sdk-6.0.100-rc.2.21505.57-osx-x64.tar.gz -C /usr/local/share"
+          } elseif ($IsLinuxEnv) {
+            $downloadUrl = "https://download.visualstudio.microsoft.com/download/pr/2b5a6c4c-5f2b-4f2b-8f1a-2c9f8f4c9f2b/2e4c6d0b0b0d0f2d4f2b0f2d4c0f4b0f/dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz"
+            $tarCommand = "tar zxf dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz -C /usr/local/share"
+          }
+          Start-Process wget -ArgumentList $downloadUrl -NoNewWindow -Wait
+          Start-Process sudo -ArgumentList $tarCommand -NoNewWindow -Wait
+          Start-Process rm -ArgumentList "dotnet-sdk-6.0.100-rc.2.21505.57*.tar.gz" -NoNewWindow -Wait
+        }
+      }
+    }
+  }
+  return $dotnetExecutable
+}
+
+# To get the OpenKNXproducer version from the executable
+function Get-OpenKNXProducerVersion {
+  $OSCommands = @{
+      "Windows" = "release/tools/Windows/OpenKNXproducer-x64.exe --version"
+      "MacOS" = "release/tools/MacOS/OpenKNXproducer-osx64 --version"
+      "Linux" = "release/tools/Linux/OpenKNXproducer-linux64 --version"
+  }
+
+  # Determine the current OS
+  $OS = "Windows"
+  if ($IsMacOSEnv) {
+      $OS = "MacOS"
+  } elseif ($IsLinuxEnv) {
+      $OS = "Linux"
+  }
+
+  # Get the version command for the current OS
+  $VersionCommand = $OSCommands[$OS]
+
+  # Make version command executable on MacOS and Linux
+  if ($OS -eq "MacOS" -or $OS -eq "Linux") {
+      Start-Process chmod -ArgumentList "+x", $VersionCommand.Split(" ")[0] -NoNewWindow -Wait
+  }
+
+  # Get version from OpenKNXproducer
+  $ReleaseName = Invoke-Expression $VersionCommand
+  return $ReleaseName
+}
 
 # Check on which OS we are running
 OpenKNX_ShowLogo "Build OpenKNXproducer Release"
 CheckOS
 
-# check for working dir
-if (Test-Path -Path release) {
-    # clean working dir
-    Remove-Item -Recurse release\* -Force
-} else {
-    New-Item -Path release -ItemType Directory | Out-Null
-}
+# Check and get dotnet executable
+$donetExecute = Test-AndInstallDotnet -Install $false
 
-# create required directories
+# check for working dir and create if not exists
+if (Test-Path -Path release) {  Remove-Item -Recurse release\* -Force
+} else { New-Item -Path release -ItemType Directory | Out-Null }
+
+# Create further required release folders
 New-Item -Path release/tools -ItemType Directory | Out-Null
 New-Item -Path release/tools/Windows -ItemType Directory | Out-Null
 New-Item -Path release/tools/MacOS -ItemType Directory | Out-Null
 New-Item -Path release/tools/Linux -ItemType Directory | Out-Null
 New-Item -Path release/tools/bossac -ItemType Directory | Out-Null
 
-# build publish version of OpenKNXproducer
-$donetExecute = "dotnet.exe"
-if($IsMacOSEnv -or $IsLinuxEnv) {
-  $donetExecute = "dotnet"
-  #check for dotnet installation and install if not available
-  if(!(Test-Path -Path /usr/local/share/dotnet/dotnet)) {
-    # Ask user if we should install dotnet
-    $installDotnet = Read-Host -Prompt "donet installation not found. Do you want to install dotnet? (y/n)"
-    if($installDotnet -eq "y") {
-      if($IsMacOSEnv) {
-        Write-Host "Install dotnet"
-        Start-Process wget -ArgumentList "https://download.visualstudio.microsoft.com/download/pr/2b5a6c4c-5f2b-4f2b-8f1a-2c9f8f4c9f2b/2e4c6d0b0b0d0f2d4f2b0f2d4c0f4b0f/dotnet-sdk-6.0.100-rc.2.21505.57-osx-x64.tar.gz" -NoNewWindow -Wait
-        Start-Process sudo -ArgumentList "tar zxf dotnet-sdk-6.0.100-rc.2.21505.57-osx-x64.tar.gz -C /usr/local/share" -NoNewWindow -Wait
-        Start-Process rm -ArgumentList "dotnet-sdk-6.0.100-rc.2.21505.57-osx-x64.tar.gz" -NoNewWindow -Wait
-      } elseif ($IsLinuxEnv) {
-        Write-Host "Install dotnet"
-        Start-Process wget -ArgumentList "https://download.visualstudio.microsoft.com/download/pr/2b5a6c4c-5f2b-4f2b-8f1a-2c9f8f4c9f2b/2e4c6d0b0b0d0f2d4f2b0f2d4c0f4b0f/dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz" -NoNewWindow -Wait
-        Start-Process sudo -ArgumentList "tar zxf dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz -C /usr/local/share" -NoNewWindow -Wait
-        Start-Process rm -ArgumentList "dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz" -NoNewWindow -Wait
-      }
-    }
-  }
-}
-
+# Build OpenKNXproducer
 Write-Host "Build OpenKNXproducer with $donetExecute"
-
 Start-Process $donetExecute -ArgumentList "build OpenKNXproducer.csproj" -NoNewWindow -Wait
 Start-Process $donetExecute -ArgumentList "publish OpenKNXproducer.csproj -c Debug -r win-x64   --self-contained true /p:PublishSingleFile=true" -NoNewWindow -Wait
 Start-Process $donetExecute -ArgumentList "publish OpenKNXproducer.csproj -c Debug -r win-x86   --self-contained true /p:PublishSingleFile=true" -NoNewWindow -Wait
 Start-Process $donetExecute -ArgumentList "publish OpenKNXproducer.csproj -c Debug -r osx-x64   --self-contained true /p:PublishSingleFile=true" -NoNewWindow -Wait
 Start-Process $donetExecute -ArgumentList "publish OpenKNXproducer.csproj -c Debug -r linux-x64 --self-contained true /p:PublishSingleFile=true" -NoNewWindow -Wait
 
-# Local build for testing
+# Local build for testing (To be discussed if and why we need this)
 if($false) {
   # we copy publish version also to our bin to ensure same OpenKNXproducer for our delivered products
   Copy-Item bin/Debug/net6.0/win-x64/publish/OpenKNXproducer.exe   ~/bin/OpenKNXproducer-x64.exe
@@ -128,39 +170,20 @@ Copy-Item -Path tools/bossac/MacOS -Destination release/tools/bossac/MacOS -Recu
 Copy-Item -Path tools/bossac/Linux -Destination release/tools/bossac/Linux -Recurse
 Copy-Item tools/bossac/LICENSE.txt release/tools/bossac/LICENSE.txt
 
-
 # add necessary scripts
 Copy-Item scripts/Readme-Release.txt release/
 Copy-Item scripts/Install-OpenKNX-Tools.ps1 release/
 
-$OSCommands = @{
-  "Windows" = "release/tools/Windows/OpenKNXproducer-x64.exe --version";
-  "MacOS" = "release/tools/MacOS/OpenKNXproducer-osx64 --version";
-  "Linux" = "release/tools/Linux/OpenKNXproducer-linux64 --version";
-}
-
-# get version command for current OS
-$OS = "Windows" 
-if($IsMacOSEnv) { 
-  $OS = "MacOS" 
-} elseif($IsLinuxEnv) {
-  $OS = "Linux"
-}
-$VersionCommand = $OSCommands[$OS]
-
-# make version command executable on MacOS and Linux
-if($OS -eq "MacOS" -or $OS -eq "Linux") {
-  Start-Process chmod -ArgumentList "+x", $VersionCommand.Split(" ")[0] -NoNewWindow -Wait
-}
-
-# get version from OpenKNXproducer
-$ReleaseName = Invoke-Expression $VersionCommand
-
-# remove spaces from release name
-$ReleaseName = $ReleaseName.Replace(" ", "-") + ".zip"
+# Get version from OpenKNXproducer and remove spaces from release name
+$ReleaseName = (Get-OpenKNXProducerVersion).Replace(" ", "-") + ".zip"
 
 # create package 
 Compress-Archive -Force -Path release/* -DestinationPath "$ReleaseName"
-#Remove-Item -Recurse release/*
+
+# Move package to release folder
 Move-Item "$ReleaseName" release/
+
+# Clean working dir
+#Remove-Item -Recurse -Force release/*
+
 OpenKNX_ShowLogo "Finished Build OpenKNXproducer Release"
