ece5486e8ceca9db68e000ebd56d366c8a96127a
Waldemar Porscha <wp@porscha.eu>
2024-01-26 15:58:58 +0100

    Release preparation:
    - change u{xxx} notation to unicode string (ps 5.1 compatibility)
    - exchange checkmark char with according unicode string
    - typo dotnet fixed
    - type --versiobn fixed

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -77,15 +77,16 @@ function Test-AndInstallDotnet {
     if (!(Test-Path -Path /usr/local/share/dotnet/dotnet)) {
       # Ask user if we should install dotnet
       if($Install) {
-        $installDotnet = Read-Host -Prompt "donet installation not found. Do you want to install dotnet? (y/n)"
-        if($installDotnet -eq "y") {
+        $installDotnet = Read-Host -Prompt "dotnet installation not found. Do you want to install dotnet? (y/n)"
+        if ($installDotnet -eq "y") {
           Write-Host "Installing dotnet..."
           $downloadUrl = ""
           $tarCommand = ""
           if ($IsMacOSEnv) {
             $downloadUrl = "https://download.visualstudio.microsoft.com/download/pr/2b5a6c4c-5f2b-4f2b-8f1a-2c9f8f4c9f2b/2e4c6d0b0b0d0f2d4f2b0f2d4c0f4b0f/dotnet-sdk-6.0.100-rc.2.21505.57-osx-x64.tar.gz"
             $tarCommand = "tar zxf dotnet-sdk-6.0.100-rc.2.21505.57-osx-x64.tar.gz -C /usr/local/share"
-          } elseif ($IsLinuxEnv) {
+          }
+          elseif ($IsLinuxEnv) {
             $downloadUrl = "https://download.visualstudio.microsoft.com/download/pr/2b5a6c4c-5f2b-4f2b-8f1a-2c9f8f4c9f2b/2e4c6d0b0b0d0f2d4f2b0f2d4c0f4b0f/dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz"
             $tarCommand = "tar zxf dotnet-sdk-6.0.100-rc.2.21505.57-linux-x64.tar.gz -C /usr/local/share"
           }
@@ -101,26 +102,26 @@ function Test-AndInstallDotnet {
 
 function Get-ApplicationVersion {
   param (
-      [string]$application = "OpenKNXproducer"
+    [string]$application = "OpenKNXproducer"
   )
 
   $OSCommands = @{
-      "Windows x86" = @{
-          "OpenKNXproducer" = "release/tools/Windows/OpenKNXproducer-x86.exe --version"
-          "bossac" = "release/tools/bossac/Windows/x86/bossac.exe --help"
-      }
-      "Windows x64" = @{
-          "OpenKNXproducer" = "release/tools/Windows/OpenKNXproducer-x64.exe --versiobn"
-          "bossac" = "release/tools/bossac/Windows/x64/bossac.exe --help"
-      }
-      "MacOS" = @{
-          "OpenKNXproducer" = "release/tools/MacOS/OpenKNXproducer --version"
-          "bossac" = "release/tools/bossac/MacOS/bossac --help"
-      }
-      "Linux" = @{
-          "OpenKNXproducer" = "release/tools/Linux/OpenKNXproducer --version"
-          "bossac" = "release/tools/bossac/Linux/bossac --help"
-      }
+    "Windows x86" = @{
+      "OpenKNXproducer" = "release/tools/Windows/OpenKNXproducer-x86.exe --version"
+      "bossac"          = "release/tools/bossac/Windows/x86/bossac.exe --help"
+    }
+    "Windows x64" = @{
+      "OpenKNXproducer" = "release/tools/Windows/OpenKNXproducer-x64.exe --version"
+      "bossac"          = "release/tools/bossac/Windows/x64/bossac.exe --help"
+    }
+    "MacOS"       = @{
+      "OpenKNXproducer" = "release/tools/MacOS/OpenKNXproducer --version"
+      "bossac"          = "release/tools/bossac/MacOS/bossac --help"
+    }
+    "Linux"       = @{
+      "OpenKNXproducer" = "release/tools/Linux/OpenKNXproducer --version"
+      "bossac"          = "release/tools/bossac/Linux/bossac --help"
+    }
   }
 
   # Determine the current OS
@@ -128,36 +129,36 @@ function Get-ApplicationVersion {
 
   # Get the version command for the current OS and application
   $VersionCommand = $OSCommands[$OS][$application]
-  if([string]::IsNullOrEmpty($VersionCommand)) {
-      Write-Host "ERROR: Could not get version command for '$application' on '$OS'" -ForegroundColor Red
-      exit 1
+  if ([string]::IsNullOrEmpty($VersionCommand)) {
+    Write-Host "ERROR: Could not get version command for '$application' on '$OS'" -ForegroundColor Red
+    exit 1
   }
 
   # Make version command executable on MacOS and Linux
   if ($OS -eq "MacOS" -or $OS -eq "Linux") {
-      $executable = $VersionCommand.Split(" ")[0]
-      if($Verbose) { Write-Host "Setting executable permissions for '$executable'." -ForegroundColor Yellow }
-      Start-Process chmod -ArgumentList "+x", $executable -NoNewWindow -Wait
+    $executable = $VersionCommand.Split(" ")[0]
+    if ($Verbose) { Write-Host "Setting executable permissions for '$executable'." -ForegroundColor Yellow }
+    Start-Process chmod -ArgumentList "+x", $executable -NoNewWindow -Wait
   }
 
   # Get version from OpenKNXproducer or bossac
-  if($Verbose) { Write-Host "Get version from '$application' with command '$VersionCommand'" -ForegroundColor Yellow }
+  if ($Verbose) { Write-Host "Get version from '$application' with command '$VersionCommand'" -ForegroundColor Yellow }
   $VersionOutput = Invoke-Expression $VersionCommand
-  if($Verbose) { Write-Host "Version output: $VersionOutput" -ForegroundColor Yellow }
+  if ($Verbose) { Write-Host "Version output: $VersionOutput" -ForegroundColor Yellow }
   
   # the patern for OPenKNXproducer is: OpenKNXproducer 'OpenKNXproducer (\d+(\.\d+)*)'
   # the patern for bossac and others is: 'Version (\d+(\.\d+)*)'
   $pattern = switch ($application) {
-      "OpenKNXproducer" { 'OpenKNXproducer (\d+(\.\d+)*)' }
-      default { 'Version (\d+(\.\d+)*)' }
+    "OpenKNXproducer" { 'OpenKNXproducer (\d+(\.\d+)*)' }
+    default { 'Version (\d+(\.\d+)*)' }
   }
-  if($Verbose) { Write-Host "Pattern: $pattern" -ForegroundColor Yellow }
+  if ($Verbose) { Write-Host "Pattern: $pattern" -ForegroundColor Yellow }
 
   $VersionMatch = ($VersionOutput | Select-String -Pattern $pattern)
   if ($VersionMatch) {
-    if($Verbose) { Write-Host "Version match: $VersionMatch" -ForegroundColor Yellow }
+    if ($Verbose) { Write-Host "Version match: $VersionMatch" -ForegroundColor Yellow }
     $Version = $VersionMatch.Matches.Groups[1].Value
-    if($Verbose) { Write-Host "Version: $Version" -ForegroundColor Yellow }
+    if ($Verbose) { Write-Host "Version: $Version" -ForegroundColor Yellow }
     return $Version
   }
   return $null
@@ -165,9 +166,9 @@ function Get-ApplicationVersion {
 
 function Invoke-DotnetExecute {
   param (
-      [string]$arguments,
-      [string]$message= "Executing dotnet command ...",
-      [string]$workingDirectory = $null
+    [string]$arguments,
+    [string]$message = "Executing dotnet command ...",
+    [string]$workingDirectory = $null
   )
 
   # Get the os specific dotnet command
@@ -182,7 +183,7 @@ function Invoke-DotnetExecute {
   $processStartInfo.CreateNoWindow = $true
 
   if ($workingDirectory) {
-      $processStartInfo.WorkingDirectory = $workingDirectory
+    $processStartInfo.WorkingDirectory = $workingDirectory
   }
 
   Write-Host $message  -ForegroundColor Green -NoNewline
@@ -192,8 +193,8 @@ function Invoke-DotnetExecute {
   $errorOutput = $process.StandardError.ReadToEnd()
 
   $process.WaitForExit()
-  Write-Host "`t✔ Done" -ForegroundColor Green
-  if($Verbose) {
+  Write-Host "`t$checkmarkChar Done" -ForegroundColor Green
+  if ($Verbose) {
     # Display the captured output for debugging purposes
     Write-Host "Standard Output: $output"
     Write-Host "Standard Error: $errorOutput"
@@ -202,20 +203,22 @@ function Invoke-DotnetExecute {
 }
 
 # Check on which OS we are running
+$infoChar = [char]::ConvertFromUtf32(0x2139);
+$checkmarkChar = [char]::ConvertFromUtf32(0x2714);
 OpenKNX_ShowLogo "Build OpenKNXproducer Release on $(CheckOS)"
 CheckOS | Out-Null
 
-if(-not $Verbose) {
-  Write-Host -NoNewline "`u{2139} " -ForegroundColor Blue
-  Write-Host -NoNewline "Verbose mode is disabled. Use -Verbose to enable it!" -ForegroundColor Blue
-  Write-Host " `u{2139}" -ForegroundColor Blue
+if (-not $Verbose) {
+  Write-Host "$infoChar Verbose mode is disabled. Use -Verbose to enable it! $infoChar" -ForegroundColor Blue
 }
 
 # check for working dir and create if not exists
 Write-Host "- Create release folder structure ..." -ForegroundColor Green -NoNewline
-if (Test-Path -Path release) {  Remove-Item -Recurse release\* -Force
-} else { New-Item -Path release -ItemType Directory | Out-Null }
-Write-Host "`t✔ Done" -ForegroundColor Green
+if (Test-Path -Path release) {
+  Remove-Item -Recurse release\* -Force
+}
+else { New-Item -Path release -ItemType Directory | Out-Null }
+Write-Host "`t$checkmarkChar Done" -ForegroundColor Green
 
 # Create further required release folders
 New-Item -Path release/tools -ItemType Directory | Out-Null
@@ -237,7 +240,7 @@ Copy-Item bin/Debug/net6.0/win-x64/publish/OpenKNXproducer.exe   release/tools/W
 Copy-Item bin/Debug/net6.0/win-x86/publish/OpenKNXproducer.exe   release/tools/Windows/OpenKNXproducer-x86.exe
 Copy-Item bin/Debug/net6.0/osx-x64/publish/OpenKNXproducer       release/tools/MacOS/OpenKNXproducer
 Copy-Item bin/Debug/net6.0/linux-x64/publish/OpenKNXproducer     release/tools/Linux/OpenKNXproducer
-Write-Host "`t✔ Done" -ForegroundColor Green
+Write-Host "`t$checkmarkChar Done" -ForegroundColor Green
 
 
 # Copy bossac tool to the release package into the dedicated folder 
@@ -246,7 +249,7 @@ Copy-Item -Path tools/bossac/Windows -Destination release/tools/bossac/Windows -
 Copy-Item -Path tools/bossac/MacOS -Destination release/tools/bossac/MacOS -Recurse
 Copy-Item -Path tools/bossac/Linux -Destination release/tools/bossac/Linux -Recurse
 Copy-Item tools/bossac/LICENSE.txt release/tools/bossac/LICENSE.txt
-Write-Host "`t✔ Done" -ForegroundColor Green
+Write-Host "`t$checkmarkChar Done" -ForegroundColor Green
 
 
 # add necessary scripts
@@ -256,7 +259,7 @@ Copy-Item scripts/Install-Application.ps1 release/tools/        # This script is
 Copy-Item scripts/Install-OpenKNXproducer.json  release/tools/  # This is the json file for the Install-OpenKNX-Tools.ps1 script
 Copy-Item scripts/Install-OpenKNX-Tools.ps1 release/            # This script is used to install the application on Windows, Linux and macOS
 Copy-Item scripts/Remove-OpenKNX-Tools.ps1 release/             # This script is used to uninstall the application on Windows, Linux and macOS
-Write-Host "`t✔ Done" -ForegroundColor Green
+Write-Host "`t$checkmarkChar Done" -ForegroundColor Green
 
 
 Write-Host "- Checking and getting versions directly from builded executables ..." -ForegroundColor Green -NoNewline
@@ -264,12 +267,14 @@ Write-Host "- Checking and getting versions directly from builded executables ..
 $OpenKNXproducerVersion = (Get-ApplicationVersion)
 $bossacVersion = (Get-ApplicationVersion -application "bossac")
 
-if( [string]::IsNullOrEmpty($OpenKNXproducerVersion) -or [string]::IsNullOrEmpty($bossacVersion) ) {
+if ( [string]::IsNullOrEmpty($OpenKNXproducerVersion) -or [string]::IsNullOrEmpty($bossacVersion) ) {
   Write-Host "ERROR: Could not get version from the executable." -ForegroundColor Red
   exit 1
-} else {
-  Write-Host "OpenKNXproducer version: $OpenKNXproducerVersion and " -ForegroundColor Green -NoNewline
-  Write-Host "bossac version: $bossacVersion `t✔ Done" -ForegroundColor Green
+}
+else {
+  Write-Host "`t$checkmarkChar Done" -ForegroundColor Green
+  Write-Host "`tOpenKNXproducer version: $OpenKNXproducerVersion" -ForegroundColor Green
+  Write-Host "`tbossac version: $bossacVersion" -ForegroundColor Green
   
   #Write the application version strings into the file version.txt. Remove version.txt if exists and create a new one.
   if (Test-Path -Path release/tools/version.txt) { Remove-Item -Path release/tools/version.txt -Force }
@@ -282,12 +287,12 @@ if( [string]::IsNullOrEmpty($OpenKNXproducerVersion) -or [string]::IsNullOrEmpty
 $ReleaseName = "OpenKNXproducer-$($OpenKNXproducerVersion).zip"
 Write-Host "- Create release package: $ReleaseName ..." -ForegroundColor Green -NoNewline
 Compress-Archive -Force -Path release/* -DestinationPath "$ReleaseName"
-Write-Host "`t✔ Done" -ForegroundColor Green
+Write-Host "`t$checkmarkChar Done" -ForegroundColor Green
 
 # Move package to release folder
 Write-Host "- Move release package to release folder ..." -ForegroundColor Green -NoNewline
 Move-Item "$ReleaseName" release/
-Write-Host "`t✔ Done" -ForegroundColor Green
+Write-Host "`t$checkmarkChar Done" -ForegroundColor Green
 
 # Clean working dir
 #Remove-Item -Recurse -Force release/*
