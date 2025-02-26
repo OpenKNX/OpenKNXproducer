8dbe27bc57228e6203f82dff4962659ac109c6ac
Mike <45664417+thewhobox@users.noreply.github.com>
2025-02-26 12:46:17 +0100

    test

--- scripts/Build-Release.ps1
+++ scripts/Build-Release.ps1
@@ -187,6 +187,7 @@ function Invoke-DotnetExecute {
   }
 
   Write-Host "Current Directory: $(Get-Location)"
+  
   Write-Host $message  -ForegroundColor Green -NoNewline
   $process = [System.Diagnostics.Process]::Start($processStartInfo)
   # Capture and suppress standard output and standard error
