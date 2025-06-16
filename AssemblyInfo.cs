7d17a55e274f3a823b865a4e4a5446355e60d27f
Waldemar Porscha <wp@porscha.eu>
2025-06-16 18:02:59 +0200

    FIX: Baggaages are not required

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -13,8 +13,8 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenKNX")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("3.7.9")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.7.9")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("3.7.10")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.7.10")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenKNXproducer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenKNXproducer")]
-[assembly: System.Reflection.AssemblyVersionAttribute("3.7.9")]
+[assembly: System.Reflection.AssemblyVersionAttribute("3.7.10")]
